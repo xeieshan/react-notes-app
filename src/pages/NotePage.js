5468be1603584d5f4e223e39e64632ebb9b88cb2
Zeeshan Haider <xeieshan@gmail.com>
2023-08-10 21:50:43 +0500

    Note Page updated

---
+++ src/pages/NotePage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import notes from "../assets/data";
+import { useParams, Link } from "react-router-dom";
+import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
+const NotePage = (props) => {
+  const params = useParams();
+  console.log("props: ", params);
+  let noteId = params.id;
+  let note = notes.find((note) => note.id === Number(noteId));
+  return (
+    <div className="note">
+      <div className="note-header">
+        <h3>
+          <Link to="/">
+            <ArrowLeft />
+          </Link>
+        </h3>
+      </div>
+      <textarea>{note.body}</textarea>
+    </div>
+  );
+};
+
+export default NotePage;
